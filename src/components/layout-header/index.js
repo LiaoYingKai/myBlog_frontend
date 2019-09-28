fbab9adc761c3385bd571874521784fc7f63ccbe
kai <b10223052@yuntech.org.tw>
2019-09-28 15:40:05 +0800

    add header style

--- src/components/layout-header/index.js
+++ src/components/layout-header/index.js
@@ -4,19 +4,25 @@ import './style.scss';
 
 function LayoutHeader() {
 	return (
-		<div className="header">
-			<ul>
-				<li>
-					<NavLink exact to="/" activeClassName="activeLink">Home</NavLink>
-				</li>
-				<li>
-					<NavLink to="/About" activeClassName="activeLink">About</NavLink>
-				</li>
-				<li>
-					<NavLink to="/Contact" activeClassName="activeLink">Contact</NavLink>
-				</li>
-			</ul>
-		</div>
+		<header className="header">
+			<div className="header__content">
+				<ul>
+					<li>
+						<NavLink exact to="/" activeClassName="activeLink">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/About" activeClassName="activeLink">About</NavLink>
+					</li>
+					<li>
+						<NavLink to="/Contact" activeClassName="activeLink">Contact</NavLink>
+					</li>
+				</ul>
+				<div className="header__user">
+					<NavLink exact to="/login">登入</NavLink>
+					<NavLink exact to="/register">註冊</NavLink>
+				</div>
+			</div>
+		</header>
 	);
 }
 
