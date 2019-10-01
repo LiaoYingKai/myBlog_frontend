69f024b1dcd5b6f31be92b95c57f3c973404f850
kai <b10223052@yuntech.org.tw>
2019-10-01 13:54:15 +0800

    modify format

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: path.join(__dirname, 'src', 'index.js'),
 	output: {
@@ -8,26 +9,31 @@ module.exports = {
 	},
 	mode: process.env.NODE_ENV || 'development',
 	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: ['babel-loader']
-		}, {
-			test: /\.(css|scss)$/,
-			use: ['style-loader', // creates style nodes from JS strings
-				'css-loader', // translates CSS into CommonJS
-				'sass-loader' // compiles Sass to CSS, using Node Sass by default
-			]
-		},{
-			test: /\.(jpe?g|png|gif|svg)$/,
-			use: [
+		rules: [
 			{
-					loader: 'url-loader',
-					options: { limit: 40000 }
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
 			},
-			'image-webpack-loader'
-			]
-		}] 
+			{
+				test: /\.(css|scss)$/,
+				use: [
+					'style-loader', // creates style nodes from JS strings
+					'css-loader', // translates CSS into CommonJS
+					'sass-loader' // compiles Sass to CSS, using Node Sass by default
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: { limit: 40000 }
+					},
+					'image-webpack-loader'
+				]
+			}
+		] 
 	},
 	resolve: {
 		modules: [path.resolve(__dirname, 'src'), 'node_modules']
