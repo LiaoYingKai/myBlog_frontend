c3bf7f328bfd692e0a5cdc2b77baa99b38c73b06
kai <b10223052@yuntech.org.tw>
2019-10-01 11:00:06 +0800

    add login page view

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,74 @@
 import React, { useState, } from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import Input from '../../components/input';
+import Button from '../../components/button';
+import TextInput from '../../components/text-input';
+import {
+	startLogin
+} from '../../actions/user-actions';
+import './style.scss';
 
-const propTypes = {};
+const {
+	SOLID,
+	HOLE,
+} = Button.TypeEnums;
 
-function Main() {
+function LoginPage() {
+	const [account, setAccount] = useState('');
+	const [password, setPassword] = useState('');
+	const dispatch = useDispatch();
+
+	function _startCreateUser() {
+		console.log(account, password);
+		// return dispatch(startLogin({ account, password }));
+	}
+	function _handleInitInputValue() {
+		setAccount('');
+		setPassword('');
+	}
+
+	function _handleSubmit() {
+		_startCreateUser();
+		_handleInitInputValue();
+	}
+
 	return (
-		<div>
-			login
+		<div className="login__container">
+			<div className="login__title">
+				登入
+			</div>
+			<TextInput
+				text={"帳號："}
+			>
+				<Input
+					placeholder={"請輸入帳號"}
+					value={account}
+					onChange={(event) => {setAccount(event.target.value);}}
+				/>
+			</TextInput>
+			<TextInput
+				text={"密碼："}
+			>
+				<Input
+					placeholder={"請輸入密碼"}
+					value={password}
+					onChange={(event) => {setPassword(event.target.value);}}
+				/>
+			</TextInput>
+			<div className="login__button-group">
+				<Button
+					text={"登入"}
+					onClick={_handleSubmit}
+					type={SOLID}
+				/>
+				<Button
+					text={"忘記密碼"}
+					onClick={_handleInitInputValue}
+					type={HOLE}
+				/>
+			</div>
 		</div>
 	);
 }
 
-Main.propTypes = propTypes;
-
-export default Main;
+export default LoginPage;
