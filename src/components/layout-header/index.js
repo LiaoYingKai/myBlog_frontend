b6f419ca5b468658abbbc109e833b8fefdd8add9
kai <b10223052@yuntech.org.tw>
2019-10-19 11:28:46 +0800

    modify route config and add enum

--- src/components/layout-header/index.js
+++ src/components/layout-header/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { NavLink, } from 'react-router-dom';
+import { PathEnums } from '../../route/config';
 import './style.scss';
+
+const {
+	HOME,
+	LOGIN,
+	REGISTER,
+	USER_EDIT,
+} = PathEnums;
 
 function LayoutHeader() {
 	return (
@@ -8,18 +16,16 @@ function LayoutHeader() {
 			<div className="header__content">
 				<ul>
 					<li>
-						<NavLink exact to="/" activeClassName="activeLink">Home</NavLink>
-					</li>
-					<li>
-						<NavLink to="/About" activeClassName="activeLink">About</NavLink>
+						<NavLink exact to={HOME} activeClassName="activeLink">首頁</NavLink>
 					</li>
 					<li>
 						<NavLink to="/Contact" activeClassName="activeLink">Contact</NavLink>
 					</li>
 				</ul>
 				<div className="header__user">
-					<NavLink exact to="/login">登入</NavLink>
-					<NavLink exact to="/register">註冊</NavLink>
+					<NavLink exact to={LOGIN}>登入</NavLink>
+					<NavLink exact to={REGISTER}>註冊</NavLink>
+					{/* <NavLink to={USER_EDIT} activeClassName="activeLink">資訊</NavLink> */}
 				</div>
 			</div>
 		</header>
--- src/route/config.js
+++ src/route/config.js
@@ -3,33 +3,46 @@ import loadComponent from './loadable';
 const Home = loadComponent({ loader: () => import('../pages/Home') });
 const Login = loadComponent({ loader: () => import('../pages/Login') });
 const Register = loadComponent({ loader: () => import('../pages/Register') });
-const About = loadComponent({ loader: () => import('../pages/About') });
+const UserEdit = loadComponent({ loader: () => import('../pages/UserEdit') });
 const Contact = loadComponent({ loader: () => import('../pages/Contact') });
 const Me =  loadComponent({ loader: () => import('../pages/Contact/Me') });
 const Another =  loadComponent({ loader: () => import('../pages/Contact/Another') });
 
+const PathEnums = {
+	HOME: '/',
+	LOGIN: '/login',
+	REGISTER: '/register',
+	USER_EDIT: '/user-edit'
+};
+
+const {
+	HOME,
+	LOGIN,
+	REGISTER,
+	USER_EDIT,
+} = PathEnums;
+
 export const config = [
 	{
-		path: '/',
+		path: HOME,
 		component: Home,
 		exact: true,
 	},
 	{
-		path: '/login',
+		path: LOGIN,
 		component: Login,
 		exact: true,
 	},
 	{
-		path: '/register',
+		path: REGISTER,
 		component: Register,
 		exact: true,
 	},
 	{
-		path: '/about',
-		component: About,
-		name: 'About',	
+		path: USER_EDIT,
+		component: UserEdit,
+		name: 'UserEdit',	
 	},
-
 	{
 		path: '/contact',
 		component: Contact,
@@ -49,3 +62,7 @@ export const config = [
 		]
 	},
 ];
+
+export {
+	PathEnums
+};
